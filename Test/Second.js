f3573fd90eb8fc3a35555c7709dee78928eaad9c
user <user@users-MacBook-Pro.local>
2024-04-23 21:46:10 +0700

    new

--- Test/Second.js
+++ Test/Second.js
@@ -1,19 +1,15 @@
 const { Builder, Browser, until, By, Key } = require('selenium-webdriver');
-
-
-(async function exemple(){
-    let driver = await new Builder().forBrowser(Browser.CHROME).build()
-
+(async function Exemple(){
+    let driver = await new Builder().forBrowser(Browser.CHROME).build();
     try{
         await driver.get('https://staging-clinic.gpet.com.vn/Login')
 
         await driver.wait(until.elementLocated(By.id('normal_login_email')),4000)
 
-        await driver.findElement(By.id('normal_login_email')).sendKeys('tien@gmail.com')
-        await driver.sleep(2000)
-        await driver.findElement(By.xpath('//*[@id="normal_login_password"]/input')).sendKeys('123456',Key.RETURN)
+        await driver.findElement(By.id('normal_login_email')).sendKeys('tientien123@gpet.vn')
+        await driver.findElement(By.xpath('//*[@id="normal_login_password"]/input')).sendKeys('hongvien',Key.RETURN)
 
-        await driver.wait(until.titleIs('Phòng Mới Cho Tiến Test'),4000)
+        await driver.wait(until.titleIs('Phòng Của Tiến'),2000)
     }finally{
         await driver.quit()
     }
